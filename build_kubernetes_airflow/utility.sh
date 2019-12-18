edc0ec9a0c1417f2078ca968764c9493cef1f4d7
apple <sanmuk21@gmail.com>
2019-12-19 00:44:29 +0100

    Fix issues in reorganisation.

--- build_kubernetes_airflow/utility.sh
+++ build_kubernetes_airflow/utility.sh
@@ -5,28 +5,37 @@ cd ${script_home}
 
 usage(){
 echo "Your need to pass one of the below option to perform below task."
-echo "- deployairflow: Deploy your Airflow Instance."
-echo "- deploystorage: Deploy all required persistent volumes."
-echo "- deployhistory: Deploy History Server Instance to view Spark Jobs."
-echo "- airflowui:     Check Airflow UI address."
-echo "- sparkui:       Check Spark Master UI."
-echo "- historyui:     Check Spark History Server UI."
+echo "- deployairflow   : Deploy your Airflow Instance."
+echo "- deploystorage   : Deploy all required persistent volumes."
+echo "- deployhistory   : Deploy History Server Instance to view Spark Jobs."
+echo "- undeployairflow : Deploy your Airflow Instance."
+echo "- undeploystorage : Deploy all required persistent volumes."
+echo "- undeployhistory : Deploy History Server Instance to view Spark Jobs."
+echo "- airflowui       : Check Airflow UI address."
+echo "- sparkui         : Check Spark Master UI."
+echo "- historyui       : Check Spark History Server UI."
 
 }
 
 case $1 in
-deployairflow) kubectl create -f airflow.yml
-               ;;;
-deploystorage) kubectl create -f storage.yml
-               ;;;
-deployhistory) kubectl create -f history.yml
-               ;;;
-airflowui)     minikube service airflow-__APP-ID__ --url -n __APP-NAMESPACE__
-               ;;;
-sparkui)       minikube service spark-master-ui-__APP-ID__ --url -n __APP-NAMESPACE__
-               ;;;
-historyui)     minikube service history-__APP-ID__ --url -n __APP-NAMESPACE__
-               ;;;
-*)             usage
-               ;;;
+deployairflow)   kubectl create -f airflow.yml
+                 ;;
+deploystorage)   kubectl create -f storage.yml
+                 ;;
+deployhistory)   kubectl create -f history.yml
+                 ;;
+undeployairflow) kubectl delete -f airflow.yml
+                 ;;
+undeploystorage) kubectl delete -f storage.yml
+                 ;;
+undeployhistory) kubectl delete -f history.yml
+                 ;;
+airflowui)       minikube service airflow-__APP-ID__ --url -n __APP-NAMESPACE__
+                 ;;
+sparkui)         minikube service spark-master-ui-__APP-ID__ --url -n __APP-NAMESPACE__
+                 ;;
+historyui)       minikube service history-__APP-ID__ --url -n __APP-NAMESPACE__
+                 ;;
+*)               usage
+                 ;;
 esac
